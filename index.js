36ea218c18c1892d166f20b44397fdb9faa6adfd
Anastassia Troitskaia <atroitskaia@gmail.com>
2017-11-14 00:10:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function fuzzyMatch (drivers, name) {
 function matchName(drivers, name) {
   return drivers.filter(
     function(driver) {
-      return driver.name.toLowerCase === name.toLowerCase
-    })
+      return driver.name.toLowerCase() === name.toLowerCase()
+    });
 }
